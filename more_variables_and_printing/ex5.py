7991496c2fc569f8b0a510f6dd3ab90517e7f5f7
Rashidul Rahul <rashidul.rahul@goama.com>
2023-01-10 22:46:33 +0600

    complete exercize 5

---
+++ more_variables_and_printing/ex5.py
@@ -0,0 +1,18 @@
+name = "Zed"
+age = 35  # not a lie
+height = 74  # inches
+weight = 180  # lbs
+eyes = "blue"
+teeth = "white"
+hair = "brown"
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pound heavy.")
+print("Actually that is not very heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on coffee.")
+
+# this line is tricky, try to get exactly right
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
